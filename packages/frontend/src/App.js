1aab8e57eb7c82ca5012e55c05412801d6dac986
Adele Gauvrit <gauvrit.adele@gmail.com>
2023-10-13 13:19:54 +0200

    implement help me button

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { HelpMe } from "./pages/HelpMe";
-import { QuestionForm } from "./pages/QuestionForm";
+import { QuestionForm } from "./components/QuestionForm";
 
 export const router = createBrowserRouter([
   {
@@ -16,10 +16,6 @@ export const router = createBrowserRouter([
   {
     path: "help",
     element: <HelpMe />,
-  },
-  {
-    path: "form",
-    element: <QuestionForm />,
   },
   {
     path: "about",
--- packages/frontend/src/pages/HelpMe.jsx
+++ packages/frontend/src/pages/HelpMe.jsx
@@ -1,12 +1,24 @@
-import * as React from "react";
 import { Menu } from "../components/Menu";
+import { QuestionForm } from "../components/QuestionForm";
+import { useState } from "react";
 
 export function HelpMe() {
+  const [iNeedHelp, setINeedHelp] = useState(false);
+  const onClick = () => {
+    setINeedHelp(true);
+  };
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <button className="p-4 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Help me
-      </button>
+      {!iNeedHelp && (
+        <button
+          className="p-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={onClick}
+        >
+          Help me 🙀🙀🙀🙀
+        </button>
+      )}
+
+      {iNeedHelp && <QuestionForm />}
       <Menu />
     </div>
   );
