e0ff965a5c72f3b1e07f5e575ab8c95695f62da6
Adele Gauvrit <gauvrit.adele@gmail.com>
2023-10-13 10:53:39 +0200

    add pages

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { HelpMe } from "./pages/HelpMe";
+import { QuestionForm } from "./pages/QuestionForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "help",
+    element: <HelpMe />,
+  },
+  {
+    path: "form",
+    element: <QuestionForm />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
---
+++ packages/frontend/src/pages/QuestionForm.jsx
@@ -0,0 +1,34 @@
+export function QuestionForm() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <form className="p-8 bg-white shadow-md rounded-md">
+        <div className="mb-4">
+          <label className="block mb-2 text-gray-700">Input ta question</label>
+          <textarea
+            className="w-full p-2 border rounded"
+            rows="3"
+            placeholder="Input ta question"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-gray-700">Match concerné</label>
+          <select className="w-full p-2 border rounded">
+            <option>Country 1 vs Country 2</option>
+            {/* Add other matches as needed */}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-gray-700">Timestamp</label>
+          <input
+            className="w-full p-2 border rounded"
+            type="text"
+            placeholder="timestamp ?"
+          />
+        </div>
+        <button className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
